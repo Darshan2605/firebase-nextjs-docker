ea014ab3cc4990a089d5af71833e816367c2a9eb
Darshan Londhe <116908065+Darshan2605@users.noreply.github.com>
2025-01-18 21:05:17 +0530

    env added and firebase congig updated

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,19 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBynz-4-HU1OZNwCnqvbwCbU_m38B36iQI",
-  authDomain: "fir-nextjs-docker.firebaseapp.com",
-  projectId: "fir-nextjs-docker",
-  storageBucket: "fir-nextjs-docker.firebasestorage.app",
-  messagingSenderId: "549516530472",
-  appId: "1:549516530472:web:887fba06e57481b12cb61d",
-  measurementId: "G-PS7VZ1Y1HL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
